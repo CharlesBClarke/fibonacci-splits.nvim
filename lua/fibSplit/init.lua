10ae0ce5625451f6fa0462a403efa646d131b0e5
CharlesBClarke <charles_b_clarke@outlook.com>
2024-12-21 21:26:53 -0800

    registered the command

--- lua/fibSplit/init.lua
+++ lua/fibSplit/init.lua
@@ -4,4 +4,12 @@ local M = {}
 
 M.fibSplit = core.fibsplit
 
+-- Register the `:fibsplit` command
+vim.api.nvim_create_user_command("Fibsplit", function(opts)
+	core.fibsplit(opts.args)
+end, {
+	nargs = "?",
+	desc = "Fibonacci Split: Splits the current window and optionally opens a file",
+})
+
 return M
